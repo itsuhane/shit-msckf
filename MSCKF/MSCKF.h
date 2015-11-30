647b2dcf5d3a921347c4824fb034b47681e6f382
Jinyu Li <itsuhane@gmail.com>
2015-11-30 16:53:42 +0800

    Add comments.

--- MSCKF/MSCKF.h
+++ MSCKF/MSCKF.h
@@ -89,7 +89,16 @@ private:
     std::vector<CameraState> m_states; // 相机 state，我们使用 R 和 T 代替论文中的 q 和 p
     std::unordered_map<size_t, std::vector<Eigen::Vector2d>> m_tracks; // 特征 tracks
 
+    // 使用 Linear LS 方法进行三角化，参考[5]
+    // 该函数求解 p 使得下面的关系对于全部的 i 成立
+    //     Proj(Rs[i]*p+Ts[i]) = xs[i]
+
+    // xs 和 m_states 从后向前对应，xs 的长度不超过 states 的长度
     Eigen::Vector3d LinearLSTriangulation(const std::vector<Eigen::Vector2d> &xs) const;
+
+    // xs.second 代表 states 中的对应项
     Eigen::Vector3d LinearLSTriangulation(const std::vector<std::pair<Eigen::Vector2d, size_t>> &xs) const;
+
+    // 根据初始值 p0 对三角化进行最小二乘优化
     Eigen::Vector3d RefineTriangulation(const Eigen::Vector3d &p0, const std::vector<std::pair<Eigen::Vector2d, size_t>> &xs) const;
 };
